7b04ff7e98fa9d04b2f6873e1f39aa723e3dc99a
aldana1311 <74303179+aldana1311@users.noreply.github.com>
2021-04-28 17:47:18 -0500

    Arreglos en form y arreglo de archivos php

---
+++ php/val_insumos.php
@@ -0,0 +1,35 @@
+<?php
+include('../includes/conection.php');
+?>
+
+<?php
+    if(isset($_POST['cre_insumo'])){
+        $tipinsumo=$_POST['tipinsumo'];
+        $nombre=$_POST['nominsumo'];
+        $marca=$_POST['marca'];
+        $color=$_POST['color'];
+        
+        
+            
+        $sql="INSERT INTO `insumos` (`ID_TIPO_INSUMO`, `NOM_INSUMOS`, `ID_MARCA`, `ID_COLOR`) VALUES ('$tipinsumo', '$nombre', '$marca', '$color')";
+            
+        $resul=mysqli_query($conexion,$sql);
+            if($resul){
+                echo "<script language='JavaScript'>
+                    alert('Se ha creado el insumo correctamente');
+                    </script>";
+                echo '<script> window.location="../crear/crear_insumo.php" </script>';
+            }else{
+                echo "<script language='JavaScript'>
+                    alert('los datos no fueron ingresados correctamente');
+                    </script>";
+                echo '<script> window.location="../crear/crear_insumo.php" </script>';
+            }
+            mysqli_close($conexion);
+    }else{
+            ?>
+   
+    
+    <?php
+    }
+    ?>
