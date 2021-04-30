2a6f4229768633465c37658e4fe9a880ffcc88a0
HOLWAN-MONTES <holmanmontesmontes57@gmail.com>
2021-04-29 23:57:17 -0500

    aaa

--- crear/crear_material.php
+++ crear/crear_material.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('../../includes/conection.php');
+include('../includes/conection.php');
 
 $usario = $_SESSION["DOCUMENTO"];
 if ($usario == "" || $usario == null) {
