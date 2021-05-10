e574e48071bbf95d27e349f63d302b3fed8a29e9
stefaniahe <62262988+stefaniahe@users.noreply.github.com>
2021-05-10 09:53:23 -0500

    formulario

---
+++ users/administrador/JS/admi.js
@@ -0,0 +1,27 @@
+// -----------------botones----------------
+const btnregi = document.getElementById("regis")
+const btnedi = document.getElementById("editaar")
+const btnelimi = document.getElementById("eliminaar")
+
+//----------------------- divs------------
+const registrar_usu = document.getElementById("regi_usu")
+const editar_usu = document.getElementById("edi_usu")
+const eliminar_usu = document.getElementById("eli_usu")
+
+btnregi.addEventListener("click", function (e) {
+    e.preventDefault();
+    registrar_usu.style.opacity = "1";
+    registrar_usu.style.visibility = "visible";
+});
+
+btnedi.addEventListener("click", function (e) {
+    e.preventDefault();
+    editar_usu.style.opacity = "1";
+    editar_usu.style.visibility = "visible";
+});
+
+btnelimi.addEventListener("click", function (e) {
+    e.preventDefault();
+    eliminar_usu.style.opacity = "1";
+    eliminar_usu.style.visibility = "visible";
+});
--- users/administrador/admin.php
+++ users/administrador/admin.php
@@ -15,7 +15,9 @@ if ($usario == "" || $usario == null) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/admi.css">
-    <link rel="stylesheet" href="CSS/editar.css">
+    <link rel="stylesheet" href="CSS/editar_usu.css">
+    <link rel="stylesheet" href="CSS/regi_usu.css">
+    <link rel="stylesheet" href="CSS/eliminar_usu.css">
     <title>Document</title>
 </head>
 <body>
@@ -33,9 +35,9 @@ if ($usario == "" || $usario == null) {
                     <ul>
                         <li class="suba_submenu" id="subm">ADMIN.USUARIOS⬇
                             <ul  class="most">
-                                <li id="reg">REGISTRO DE USUARIOS</li>
-                                <li>EDITAR USUARIOS</a></li>
-                                <li>ELIMINAR USUARIOS</li>
+                                <li id="regis">REGISTRO DE USUARIOS</li>
+                                <li id="editaar">EDITAR USUARIOS</a></li>
+                                <li id="eliminaar">ELIMINAR USUARIOS</li>
                             </ul>
                         </li>
                         <li class="suba_submenu" id="subm">REGISTRO ⬇
@@ -83,10 +85,89 @@ if ($usario == "" || $usario == null) {
             <div class="info">
 
                 <div class="usuariooo">
-                    <div class="regusu">
+                    <div class="regusu" id="regi_usu">
+                        <div class="titulo">
+                            <h1>Registro De Usuarios</h1>
+                        </div>
+                        <form class="form" action="../../php/crear.php" method="POST">
+                            <input type="number" name="docu" id="docu" placeholder="DOCUMENTO" autocomplete="off" required> &nbsp;&nbsp;&nbsp;
+                            <input type="text" name="nom" id="nom" placeholder="NOMBRE" autocomplete="off" required style="text-transform:uppercase">&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="text" name="apel" id="apel" placeholder="APELLIDO" autocomplete="off" required style="text-transform:uppercase"> <br><br>
+                            <div class="user">
+                                <label id="tex-use" for="">TIPO DE USUARIO</label><br>
+                                <select name="tip_us" class="tip_usu" id="tip_usu" autocomplete="off" required>
+                                    <option value="">SELECCIONAR</option>
+                                    <?php
+                                        $tipo = "SELECT * FROM tipo_usu";
+                                        $inser = mysqli_query($conexion,$tipo);
+                                        while($tip = mysqli_fetch_array($inser)){
+                                    ?>
+                                    <option name="tip_user" value="<?php echo $tip[0]; ?>" style="text-transform:uppercase"><?php echo $tip[1]; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select><br>
+                                <a class="crear-user" href="">CREAR TIPO DE USUARIO</a>
+                            </div><br>
+                            <div class="doc">
+                                <label class="tipo-doc" for="">TIPO DE DOCUMENTO</label><br>
+                                <select name="tip_doc" id="tip_docu" autocomplete="off" required>
+                                    <option value="">SELECCIONAR</option>
+                                    <?php
+                                        $tipo2 = "SELECT * FROM tipo_docu";
+                                        $inser2 = mysqli_query($conexion,$tipo2);
+                                        while($tip2 = mysqli_fetch_array($inser2)){
+                                    ?>
+                                    <option name="tip_user" value="<?php echo $tip2[0]; ?>" style="text-transform:uppercase"><?php echo $tip2[1]; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select><br>
+                                <a class="crear-doc" href="">CREAR TIPO DE DOCUMENTO</a>
+                            </div><br>
+                            
+                            <input type="number" name="edad" id="edad" placeholder="EDAD" autocomplete="off" min="1" max="100" required>
+                            <input type="password" name="contra" id="contra" placeholder="CONTRASEÑA" autocomplete="off" pattern="[A-Za-z0-9!?-]{2,12}" required>&nbsp;&nbsp;&nbsp;
+                            <input type="number" name="tele" id="tele" placeholder="TELEFONO" autocomplete="off" min="1" max="3999999999" required>
+                            <input type="email" name="cor" id="cor" placeholder="CORREO" autocomplete="off" pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required><br>
+                            <input type="submit" name="registro" id="reg" value="REGISTRAR">
+                        </form>
                         
+                        <div class="ven1">
+                            <div class="ventana-modal1" id="ventana-modal1">
+                                <div class="modal1 modal-close1">
+                                    <a href=""><img class="cerrar1" src="../../img/cerrar.png" alt="Cerrar"></a>
+                                    <div class="modal-text" id="modal-text">
+                                        <h2 class="reg-user" id="reg-user">Agregar Tip. Usuario</h2>
+                                        <form class="formul" action="../../php/registro_users_doc.php" method="post">
+                                            <label class="dig-user" for="">Digite Tipo Usuario</label><br><br>
+                                            <input type="text" class="ti-user" name="tipo-user" placeholder="TIPO USUARIO" autocomplete="off" required><br><br>
+                                            <input type="submit" name="env-user" class="env-user" value="AGREGAR">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ven">
+                            <div class="ventana-modal" id="ventana-modal">
+                                <div class="modal modal-close">
+                                    <a href=""><img class="cerrar" src="../../img/cerrar.png" alt="Cerrar"></a>
+                                    <div class="modal-text" id="modal-text">
+                                        <h2 class="reg-doc" id="reg-doc">Agregar Tip. Documento</h2>
+                                        <form class="formu" action="../../php/registro_users_doc.php" method="post">
+                                            <label class="dig-doc" for="">Digite Tipo Documento</label><br><br>
+                                            <input type="text" class="ti-docu" name="tipo-docume" placeholder="TIPO DOCUMENTO" autocomplete="off" required><br><br>
+                                            <input type="submit" name="env-doc" class="env-doc" value="AGREGAR">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="ediusu">
+
+
+                    <div class="ediusu" id="edi_usu">
                         <form class="form" id="form" method="POST">
                             <input type="number" name="docu" id="docu" placeholder="DOCUMENTO" autocomplete="off" required> &nbsp;&nbsp;&nbsp;
                             <input type="text" name="nom" id="nom" placeholder="NOMBRE" autocomplete="off" required>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -130,9 +211,55 @@ if ($usario == "" || $usario == null) {
                             <input type="submit" name="actualiza" id="edi" value="EDITAR">
                         </form>
                     </div>
-                    <div class="elimiusu">
 
+
+                    <div class="elimiusu" id="eli_usu">
+                        <div class="titulo">
+                            <h1>Eliminación De Usuarios</h1>
+                        </div>
+                        <form class="form" id="form" method="POST">
+                            <input type="number" name="docu" id="docu" placeholder="DOCUMENTO" autocomplete="off" required> &nbsp;&nbsp;&nbsp;
+                            <input type="text" name="nom" id="nom" placeholder="NOMBRE" autocomplete="off" required>&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="text" name="apel" id="apel" placeholder="APELLIDO" autocomplete="off" required> <br><br>
+                            <div class="user">
+                                <label id="tex-use" for="">TIPO DE USUARIO</label><br>
+                                <select name="tip_us" class="tip_usu" id="tip_usu" autocomplete="off" required>
+                                    <option value="0">SELECCIONAR</option>
+                                    <?php
+                                        $tipo = "SELECT * FROM tipo_usu";
+                                        $inser = mysqli_query($conexion,$tipo);
+                                        while($tip = mysqli_fetch_array($inser)){
+                                    ?>
+                                    <option name="tip_user" value="<?php echo $tip[0]; ?>"><?php echo $tip[1]; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select><br>
+                            </div>
+                            <div class="doc">
+                                <label for="">TIPO DE DOCUMENTO</label><br>
+                                <select name="tip_doc" id="tip_docu" autocomplete="off" required>
+                                    <option value="0">SELECCIONAR</option>
+                                    <?php
+                                        $tipo2 = "SELECT * FROM tipo_docu";
+                                        $inser2 = mysqli_query($conexion,$tipo2);
+                                        while($tip2 = mysqli_fetch_array($inser2)){
+                                    ?>
+                                    <option name="tip_user" value="<?php echo $tip2[0]; ?>"><?php echo $tip2[1]; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div><br>
+                            
+                            <input type="number" name="edad" id="edad" placeholder="EDAD" autocomplete="off" required>
+                            <input type="number" name="tele" id="tele" placeholder="TELEFONO" autocomplete="off" required> <br>
+                            <input type="text" name="cor" id="cor" placeholder="CORREO" autocomplete="off" required><br>
+                            <input type="hidden" name="docume" id="docume">
+                            <input type="submit" name="eliminar" id="elimi" value="ELIMINAR">
+                        </form>
                     </div>
+
                 </div>
 
             </div>
@@ -141,5 +268,8 @@ if ($usario == "" || $usario == null) {
 
     </div>
     <script src="JS/editar.js"></script>
+    <script src="JS/main.js"></script>
+    <script src="JS/admi.js"></script>
+    <script src="JS/eliminar_usu.js"></script>
 </body>
 </html>
