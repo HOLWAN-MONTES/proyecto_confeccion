9c03b6d2043b6bb56ea50bc1a6ea549bcd6b683e
stefaniahe <62262988+stefaniahe@users.noreply.github.com>
2021-05-12 08:33:58 -0500

    formularios

--- users/administrador/JS/admi.js
+++ users/administrador/JS/admi.js
@@ -12,16 +12,19 @@ btnregi.addEventListener("click", function (e) {
     e.preventDefault();
     registrar_usu.style.opacity = "1";
     registrar_usu.style.visibility = "visible";
+    registrar_usu.style.display="block";
 });
 
 btnedi.addEventListener("click", function (e) {
     e.preventDefault();
     editar_usu.style.opacity = "1";
     editar_usu.style.visibility = "visible";
+    editar_usu.style.display="block";
 });
 
 btnelimi.addEventListener("click", function (e) {
     e.preventDefault();
     eliminar_usu.style.opacity = "1";
     eliminar_usu.style.visibility = "visible";
+    eliminar_usu.style.display="block";
 });
--- users/administrador/admin.php
+++ users/administrador/admin.php
@@ -15,8 +15,8 @@ if ($usario == "" || $usario == null) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/admi.css">
-    <link rel="stylesheet" href="CSS/editar_usu.css">
     <link rel="stylesheet" href="CSS/regi_usu.css">
+    <link rel="stylesheet" href="CSS/editar_usu.css"> 
     <link rel="stylesheet" href="CSS/eliminar_usu.css">
     <title>Document</title>
 </head>
@@ -84,86 +84,90 @@ if ($usario == "" || $usario == null) {
 
             <div class="info">
 
-                <div class="usuariooo">
+                <div class="usuar">
                     <div class="regusu" id="regi_usu">
-                        <div class="titulo">
-                            <h1>Registro De Usuarios</h1>
-                        </div>
-                        <form class="form" action="../../php/crear.php" method="POST">
-                            <input type="number" name="docu" id="docu" placeholder="DOCUMENTO" autocomplete="off" required> &nbsp;&nbsp;&nbsp;
-                            <input type="text" name="nom" id="nom" placeholder="NOMBRE" autocomplete="off" required style="text-transform:uppercase">&nbsp;&nbsp;&nbsp;&nbsp;
-                            <input type="text" name="apel" id="apel" placeholder="APELLIDO" autocomplete="off" required style="text-transform:uppercase"> <br><br>
-                            <div class="user">
-                                <label id="tex-use" for="">TIPO DE USUARIO</label><br>
-                                <select name="tip_us" class="tip_usu" id="tip_usu" autocomplete="off" required>
-                                    <option value="">SELECCIONAR</option>
-                                    <?php
-                                        $tipo = "SELECT * FROM tipo_usu";
-                                        $inser = mysqli_query($conexion,$tipo);
-                                        while($tip = mysqli_fetch_array($inser)){
-                                    ?>
-                                    <option name="tip_user" value="<?php echo $tip[0]; ?>" style="text-transform:uppercase"><?php echo $tip[1]; ?></option>
-                                    <?php
-                                    }
-                                    ?>
-                                </select><br>
-                                <a class="crear-user" href="">CREAR TIPO DE USUARIO</a>
-                            </div><br>
-                            <div class="doc">
-                                <label class="tipo-doc" for="">TIPO DE DOCUMENTO</label><br>
-                                <select name="tip_doc" id="tip_docu" autocomplete="off" required>
-                                    <option value="">SELECCIONAR</option>
-                                    <?php
-                                        $tipo2 = "SELECT * FROM tipo_docu";
-                                        $inser2 = mysqli_query($conexion,$tipo2);
-                                        while($tip2 = mysqli_fetch_array($inser2)){
-                                    ?>
-                                    <option name="tip_user" value="<?php echo $tip2[0]; ?>" style="text-transform:uppercase"><?php echo $tip2[1]; ?></option>
-                                    <?php
-                                    }
-                                    ?>
-                                </select><br>
-                                <a class="crear-doc" href="">CREAR TIPO DE DOCUMENTO</a>
-                            </div><br>
-                            
-                            <input type="number" name="edad" id="edad" placeholder="EDAD" autocomplete="off" min="1" max="100" required>
-                            <input type="password" name="contra" id="contra" placeholder="CONTRASEÑA" autocomplete="off" pattern="[A-Za-z0-9!?-]{2,12}" required>&nbsp;&nbsp;&nbsp;
-                            <input type="number" name="tele" id="tele" placeholder="TELEFONO" autocomplete="off" min="1" max="3999999999" required>
-                            <input type="email" name="cor" id="cor" placeholder="CORREO" autocomplete="off" pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required><br>
-                            <input type="submit" name="registro" id="reg" value="REGISTRAR">
-                        </form>
+                        <br>
                         
-                        <div class="ven1">
-                            <div class="ventana-modal1" id="ventana-modal1">
-                                <div class="modal1 modal-close1">
-                                    <a href=""><img class="cerrar1" src="../../img/cerrar.png" alt="Cerrar"></a>
-                                    <div class="modal-text" id="modal-text">
-                                        <h2 class="reg-user" id="reg-user">Agregar Tip. Usuario</h2>
-                                        <form class="formul" action="../../php/registro_users_doc.php" method="post">
-                                            <label class="dig-user" for="">Digite Tipo Usuario</label><br><br>
-                                            <input type="text" class="ti-user" name="tipo-user" placeholder="TIPO USUARIO" autocomplete="off" required><br><br>
-                                            <input type="submit" name="env-user" class="env-user" value="AGREGAR">
-                                        </form>
+                        <div class="regg">
+                            <div class="tituloos">
+                                <h1>REGISTRO DE USUARIOS</h1>
+                            </div>
+                            <form class="form1" action="../../php/crear.php" method="POST">
+                                <input type="number" name="docu" id="docu" placeholder="DOCUMENTO" autocomplete="off" required> &nbsp;&nbsp;&nbsp;
+                                <input type="text" name="nom" id="nom" placeholder="NOMBRE" autocomplete="off" required style="text-transform:uppercase">&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input type="text" name="apel" id="apel" placeholder="APELLIDO" autocomplete="off" required style="text-transform:uppercase"> <br><br>
+                                <div class="user">
+                                    <label id="tex-use" for="">TIPO DE USUARIO</label><br>
+                                    <select name="tip_us" class="tip_usu" id="tip_usu" autocomplete="off" required>
+                                        <option value="">SELECCIONAR</option>
+                                        <?php
+                                            $tipo = "SELECT * FROM tipo_usu";
+                                            $inser = mysqli_query($conexion,$tipo);
+                                            while($tip = mysqli_fetch_array($inser)){
+                                        ?>
+                                        <option name="tip_user" value="<?php echo $tip[0]; ?>" style="text-transform:uppercase"><?php echo $tip[1]; ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </select><br>
+                                    <a class="crear-user" href="">CREAR TIPO DE USUARIO</a>
+                                </div><br>
+                                <div class="doc">
+                                    <label class="tipo-doc" for="">TIPO DE DOCUMENTO</label><br>
+                                    <select name="tip_doc" id="tip_docu" autocomplete="off" required>
+                                        <option value="">SELECCIONAR</option>
+                                        <?php
+                                            $tipo2 = "SELECT * FROM tipo_docu";
+                                            $inser2 = mysqli_query($conexion,$tipo2);
+                                            while($tip2 = mysqli_fetch_array($inser2)){
+                                        ?>
+                                        <option name="tip_user" value="<?php echo $tip2[0]; ?>" style="text-transform:uppercase"><?php echo $tip2[1]; ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </select><br>
+                                    <a class="crear-doc" href="">CREAR TIPO DE DOCUMENTO</a>
+                                </div><br>
+                                
+                                <input type="number" name="edad" id="edad" placeholder="EDAD" autocomplete="off" min="1" max="100" required>
+                                <input type="password" name="contra" id="contra" placeholder="CONTRASEÑA" autocomplete="off" pattern="[A-Za-z0-9!?-]{2,12}" required>&nbsp;&nbsp;&nbsp;
+                                <input type="number" name="tele" id="tele" placeholder="TELEFONO" autocomplete="off" min="1" max="3999999999" required>
+                                <input type="email" name="cor" id="cor" placeholder="CORREO" autocomplete="off" pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required><br>
+                                <input type="submit" name="registro" id="reg" value="REGISTRAR">
+                            </form>
+                            <div class="ven1">
+                                <div class="ventana-modal1" id="ventana-modal1">
+                                    <div class="modal1 modal-close1">
+                                        <a href=""><img class="cerrar1" src="../../img/cerrar.png" alt="Cerrar"></a>
+                                        <div class="modal-text" id="modal-text">
+                                            <h2 class="reg-user" id="reg-user">Agregar Tip. Usuario</h2>
+                                            <form class="formul" action="../../php/registro_users_doc.php" method="post">
+                                                <label class="dig-user" for="">Digite Tipo Usuario</label><br><br>
+                                                <input type="text" class="ti-user" name="tipo-user" placeholder="TIPO USUARIO" autocomplete="off" required><br><br>
+                                                <input type="submit" name="env-user" class="env-user" value="AGREGAR">
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="ven">
-                            <div class="ventana-modal" id="ventana-modal">
-                                <div class="modal modal-close">
-                                    <a href=""><img class="cerrar" src="../../img/cerrar.png" alt="Cerrar"></a>
-                                    <div class="modal-text" id="modal-text">
-                                        <h2 class="reg-doc" id="reg-doc">Agregar Tip. Documento</h2>
-                                        <form class="formu" action="../../php/registro_users_doc.php" method="post">
-                                            <label class="dig-doc" for="">Digite Tipo Documento</label><br><br>
-                                            <input type="text" class="ti-docu" name="tipo-docume" placeholder="TIPO DOCUMENTO" autocomplete="off" required><br><br>
-                                            <input type="submit" name="env-doc" class="env-doc" value="AGREGAR">
-                                        </form>
+                            <div class="ven">
+                                <div class="ventana-modal" id="ventana-modal">
+                                    <div class="modal modal-close">
+                                        <a href=""><img class="cerrar" src="../../img/cerrar.png" alt="Cerrar"></a>
+                                        <div class="modal-text" id="modal-text">
+                                            <h2 class="reg-doc" id="reg-doc">Agregar Tip. Documento</h2>
+                                            <form class="formu" action="../../php/registro_users_doc.php" method="post">
+                                                <label class="dig-doc" for="">Digite Tipo Documento</label><br><br>
+                                                <input type="text" class="ti-docu" name="tipo-docume" placeholder="TIPO DOCUMENTO" autocomplete="off" required><br><br>
+                                                <input type="submit" name="env-doc" class="env-doc" value="AGREGAR">
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <img src="../../img/fondo.jpg" alt="">
                     </div>
 
 
