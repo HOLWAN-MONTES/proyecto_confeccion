f26e7e4e247aec8b5ba708f70469cf2f2b61c6c4
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-05-26 11:13:27 -0500

    Revert "Merge branch 'master' into gabriela"
    This reverts commit 2be06d2a07ee643bca5c29b26c658ae593248d1a, reversing
    changes made to 96ee062ddcbc6a49b5d41ce849639b9aa93cafd8.

--- users/administrador/JS/admi.js
+++ users/administrador/JS/admi.js
@@ -92,8 +92,6 @@ btninsu.addEventListener("click", function (e) {
     crea_maquina.style.opacity = "0";
     crea_maquina.style.visibility = "hidden";
     crea_maquina.style.display="none";
-
-
     
 });
 btnmatextil.addEventListener("click", function (e) {
