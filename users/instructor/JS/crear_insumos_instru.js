4a9f8b0cd3d853aac1b1e32e91202a63a3417afd
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-06-09 21:52:27 -0500

    ventanas modales del instructor

--- users/instructor/JS/crear_insumos_instru.js
+++ users/instructor/JS/crear_insumos_instru.js
@@ -11,26 +11,26 @@ cerrar_ventana.addEventListener('click',function(){
     crear_tipo_tela.style.display = "none";
 });
 //ventana de registro de la marca del insumo
-const btn_salirmarca = document.getElementById('btn_salirmarca');
-const crear_marca = document.getElementById('crear_marca');
-const cerrar_ventanaMarca = document.getElementById('cerrar_ventanaMarca');
+const btn_salirmarca_insu = document.getElementById('btn_salirmarca_insu');
+const crear_marca_ins = document.getElementById('crear_marca_ins');
+const cerrar_ventanaMarca_ins = document.getElementById('cerrar_ventanaMarca_ins');
 
-btn_salirmarca.addEventListener('click',function(){
-    crear_marca.style.display = "block";
+btn_salirmarca_insu.addEventListener('click',function(){
+    crear_marca_ins.style.display = "block";
 });
 
-cerrar_ventanaMarca.addEventListener('click',function(){
-    crear_marca.style.display = "none";
+cerrar_ventanaMarca_ins.addEventListener('click',function(){
+    crear_marca_ins.style.display = "none";
 });
 //ventana de registrar el color del insumo
-const btn_salircolor = document.getElementById('btn_salircolor');
-const crear_color = document.getElementById('crear_color');
-const cerrar_ventanaColor = document.getElementById('cerrar_ventanaColor');
+const btn_salircolor_insu = document.getElementById('btn_salircolor_insu');
+const crear_color_insu = document.getElementById('crear_color_insu');
+const cerrar_ventanaColor_insu = document.getElementById('cerrar_ventanaColor_insu');
 
-btn_salircolor.addEventListener('click',function(){
-    crear_color.style.display = "block";
+btn_salircolor_insu.addEventListener('click',function(){
+    crear_color_insu.style.display = "block";
 });
 
-cerrar_ventanaColor.addEventListener('click',function(){
-    crear_color.style.display = "none";
+cerrar_ventanaColor_insu.addEventListener('click',function(){
+    crear_color_insu.style.display = "none";
 });
