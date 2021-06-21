378803632f4e8642a636e410e17f96a44bbc65c2
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-06-21 01:56:35 -0500

    paginas del inicio

--- js/index.js
+++ js/index.js
@@ -1,14 +1,49 @@
-const boton_Sobre = document.getElementById("dos")
-const sobre_noso = document.getElementById("sobrenoso")
-const princi = document.getElementById("princi")
+//botones de las paginas 
+const boton_Sobre = document.getElementById("dos");
+const boton_servicios = document.getElementById("tres");
+const boton_contactenos = document.getElementById("cuatro");
+
+//imagen y div principal
+const princi = document.getElementById("princi");
+const imagen = document.getElementById("imagen");
+
+//contenido de las paginas 
+const sobre_noso = document.getElementById("sobrenoso");
+const servicios = document.getElementById("servicios");
+const contactar = document.getElementById("contactar");
 
 boton_Sobre.addEventListener('click', (e)=>{
     e.preventDefault();
-    // fond.style.opacity="0";
-    // mostrardoc.style.background="#238276";
     sobre_noso.style.visibility="visible"; 
-    princi.style.visibility="hidden" ;
-})
+    sobre_noso.style.opacity= "1";
+    sobre_noso.style.display = "block";
+    princi.style.display="none";
+    imagen.style.display = "none";
+    servicios.style.display="none";
+    contactar.style.display="none";    
+});
+
+boton_servicios.addEventListener('click', (e)=>{
+    e.preventDefault();
+    servicios.style.display="block";
+    servicios.style.opacity = "1";
+    servicios.style.visibility="visible";
+    princi.style.display="none";
+    imagen.style.display="none";
+    sobre_noso.style.display="none";
+    contactar.style.display="none";
+});
+
+boton_contactenos.addEventListener('click', (e)=>{
+    contactar.style.display="block";
+    contactar.style.opacity="1";
+    contactar.style.visibility="visible";
+    princi.style.display="none";
+    imagen.style.display="none";
+    sobre_noso.style.display="none";
+    servicios.style.display="none";
+
+});
 
 
 
